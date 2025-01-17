b24c6b4a5279d9c7ef1e8aec1e7183bb88867bbc
tahyeokkim89 <wskth0930@gmail.com>
2025-01-17 16:22:00 +0900

    vanillaextract 추가

---
+++ packages/react/components/layout/build.js
@@ -0,0 +1,11 @@
+import run from "@th/esbuild-config";
+import pkg from './package.json' assert {type: 'json'};
+import {vanillaExtractPlugin} from '@vanilla-extract/esbuild-plugin'
+
+const config = {
+  plugins: [vanillaExtractPlugin()]
+};
+run({
+  pkg,
+  config
+})
